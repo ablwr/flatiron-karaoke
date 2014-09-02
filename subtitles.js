b608c83567361a725e6d495fb037de845262885c
ablwr <ashley.blewer@gmail.com>
2014-09-01 23:18:09 -0400

    lyrics

--- subtitles.js
+++ subtitles.js
@@ -1,16 +1,81 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-   var example = Popcorn.youtube(
+   var pop = Popcorn.youtube(
      '#video',
      'https://www.youtube.com/watch?v=i4tTnIimi8E' );
 
-   example.footnote({
-     start: 2,
-     end: 6,
-     text: "Pop!",
-     target: "footnotediv"
+   pop.footnote({
+     [{start: 2,
+               end: 6,
+               text: "Pop!",
+               target: "footnotediv"},
+          
+{      start: 16,
+      end: 17,
+      text: "There used to be",
+      target: "footnotediv"}]
    });
 
-   example.play();
+   pop.play();
 
 }, false);
+
+
+
+// There used to be a graying tower alone on the sea.
+// You became the light on the dark side of me.
+// Love remained a drug that's the high and not the pill.
+// But did you know,
+// That when it snows,
+// My eyes become large and
+// The light that you shine can be seen.
+// Baby,
+// I compare you to a kiss from a rose on the gray.
+// Ooh,
+// The more I get of you,
+// The stranger it feels, yeah.
+// And now that your rose is in bloom.
+// A light hits the gloom on the gray.
+// There is so much a man can tell you,
+// So much he can say.
+// You remain,
+// My power, my pleasure, my pain, baby
+// To me you're like a growing addiction that I can't deny.
+// Won't you tell me is that healthy, baby?
+// But did you know,
+// That when it snows,
+// My eyes become large and the light that you shine can be seen.
+// Baby,
+// I compare you to a kiss from a rose on the gray.
+// Ooh, the more I get of you
+// The stranger it feels, yeah
+// Now that your rose is in bloom.
+// A light hits the gloom on the gray,
+// I've been kissed by a rose on the gray,
+// I've been kissed by a rose
+// I've been kissed by a rose on the gray,
+// ...And if I should fall along the way
+// I've been kissed by a rose
+// ...been kissed by a rose on the gray.
+// There is so much a man can tell you,
+// So much he can say.
+// You remain
+// My power, my pleasure, my pain.
+// To me you're like a growing addiction that I can't deny, yeah
+// Won't you tell me is that healthy, baby.
+// But did you know,
+// That when it snows,
+// My eyes become large and the light that you shine can be seen.
+// Baby,
+// I compare you to a kiss from a rose on the gray.
+// Ooh, the more I get of you
+// The stranger it feels, yeah
+// Now that your rose is in bloom,
+// A light hits the gloom on the gray.
+// Yes I compare you to a kiss from a rose on the gray
+// Ooh, the more I get of you
+// The stranger it feels, yeah
+// And now that your rose is in bloom
+// A light hits the gloom on the gray
+// Now that your rose is in bloom,
+// A light hits the gloom on the gray.
